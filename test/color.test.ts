9254085a08bf4b973c248ba3d4ab47162377d9e9
lightyen <lightyen0123@gmail.com>
2022-05-19 12:44:43 +0800

    more test

--- test/color.test.ts
+++ test/color.test.ts
@@ -1,6 +1,11 @@
 import { blend, parseColor } from "../src/colors"
 
 it("parse", () => {
+	expect(parseColor("#365842")).toEqual(0x365842)
+	expect(parseColor("rgb(54, 88, 66)")).toEqual(0x365842)
+	expect(parseColor("rgb(54 88 66)")).toEqual(0x365842)
+	expect(parseColor("rgba(54, 88, 66, 0.5)")).toEqual(0x365842)
+	expect(parseColor("rgba(54  88  66/ 0.5)")).toEqual(0x365842)
 	expect(parseColor("hsl( 357, 60%, 57%)")).toEqual(parseColor("rgb(211 80  86)"))
 })
 
